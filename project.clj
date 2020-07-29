0f4a473b5eb291f666db46d7700033263155ccd3
Alexander Oloo <alekcz@gmail.com>
2020-07-29 13:34:50 +0200

    update http-kit

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject alekcz/fire "0.2.10"
+(defproject alekcz/fire "0.2.11-SNAPSHOT"
   :description "Firebase from Clojure. Basically Charmander 2.0"
   :url "https://github.com/alekcz/fire"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [ [org.clojure/clojure "1.10.0"]
                   [alekcz/googlecredentials "3.0.1"]
-                  [http-kit "2.4.0-alpha6"]
+                  [http-kit "2.4.0-RC1"]
                   [cheshire "5.10.0"]
                   [org.clojure/core.async "1.1.587"]]
   :plugins [[lein-cloverage "1.1.2"]]
