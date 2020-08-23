6be9115d45a2ff98fd0e5cc9076728fc11360ff7
Alexander Oloo <alekcz@gmail.com>
2020-08-24 00:07:33 +0200

    new clojure

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/alekcz/fire"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [ [org.clojure/clojure "1.9.0"]
+  :dependencies [ [org.clojure/clojure "1.10.2-alpha1"]
                   [http-kit "2.5.0-alpha2"]
                   [cheshire "5.10.0"]
                   [environ "1.2.0"]
