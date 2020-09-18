9b38adf1ba404018e98d62862dd8ed7f63cd11ad
Alexander Oloo <alekcz@gmail.com>
2020-09-18 22:15:56 +0200

    update http-kit version

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [ [org.clojure/clojure "1.10.2-alpha1"]
-                  [http-kit "2.5.0-alpha2"]
+                  [http-kit "2.5.0"]
                   [cheshire "5.10.0"]
                   [environ "1.2.0"]
                   [org.clojure/core.async "1.1.587"]]
