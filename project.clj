9163424680dc798e25aede95c7a14f735d1b6d0c
Alex Oloo <alekcz@gmail.com>
2021-07-07 22:48:59 +0200

    update deps

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
-(defproject alekcz/fire "0.4.0"
+(defproject alekcz/fire "0.4.1"
   :description "Firebase from Clojure. Basically Charmander 2.0"
   :url "https://github.com/alekcz/fire"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [ [org.clojure/clojure "1.10.3" :scope "provided"]
-                  [http-kit "2.5.0"]
+                  [org.clojure/core.async "1.3.618"]
+                  [http-kit "2.5.3"]
                   [cheshire "5.10.0"]
                   [environ "1.2.0"]
-                  [org.clojure/core.async "1.1.587"]
                   [stylefruits/gniazdo "1.2.0"]
                   [danlentz/clj-uuid "0.1.9"]]
   :plugins [[lein-cloverage "1.2.2"]
@@ -33,6 +33,7 @@
     "--allow-incomplete-classpath"
     "--initialize-at-build-time"
     "--no-fallback"
+    "--initialize-at-run-time=org.httpkit.client.ClientSslEngineFactory\\$SSLHolder"
     "--enable-url-protocols=http,https"
     "-jar" "./target/${:uberjar-name:-${:name}-${:version}-standalone.jar}"
     "-H:Name=./target/${:name}"]
