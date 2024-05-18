9421dc27eaa351b8f1de42cf80c41457a43ce67a
Alex Oloo <alekcz@gmail.com>
2024-05-18 08:07:15 +0200

    update deps

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "https://github.com/alekcz/fire"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [ [org.clojure/clojure "1.11.1"]
-                  [org.clojure/core.async "1.5.648"]
-                  [http-kit "2.6.0"]
-                  [cheshire "5.11.0"]
+  :dependencies [ [org.clojure/clojure "1.11.3"]
+                  [org.clojure/core.async "1.6.681"]
+                  [http-kit "2.8.0"]
+                  [cheshire "5.13.0"]
                   [environ "1.2.0"]
                   [stylefruits/gniazdo "1.2.1"]
                   [danlentz/clj-uuid "0.1.9"]
